1b5c2dc91d04ae89d1b4314d7f393c6e7fd3b51d
Farhan Islam <bsse1303@iit.du.ac.bd>
2025-04-28 14:59:00 +0600

    refactor: start from scratch

--- src/app.js
+++
@@ -1,7 +0,0 @@
-import Game from './game/Game.js';
-
-// Initialize the game when DOM is loaded
-document.addEventListener('DOMContentLoaded', () => {
-    const game = new Game();
-    game.start();
-});
